2d5ec882f46629c92efeccf2fd15478a4ad4c6b5
Jonas Sørensen <jonas7793@gmail.com>
2021-03-30 10:49:54 +0200

    Login Logic & Redirect to Chatroom

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,24 +2,32 @@
   <form @submit.prevent="handleSubmit">
       <input type="email" required placeholder="E-mail" v-model="email">
       <input type="password" required placeholder="********" v-model="password">
+      <div class="error">{{ error }}</div>
       <button>Login</button>
   </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+import useLogin from '../composables/useLogin'
+
 export default {
-    setup() {
+    setup(props, context) {
         // refs
         const email = ref('')
         const password = ref('')
 
-        const handleSubmit = () => {
-            console.log(email.value, password.value)
+        const { error, login } = useLogin() 
+
+        const handleSubmit = async () => {
+            await login(email.value, password.value)
+            if (!error.value) {
+                context.emit('login')
+            }
         }
 
         return {
-            email, password, handleSubmit
+            email, password, handleSubmit, error
         }
     }
 }
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,12 +3,12 @@
       <p>Welcome</p>
       <div v-if="showLogin">
         <h2>Login</h2>
-        <LoginForm />
+        <LoginForm @login="enterChat" />
         <p>No account yet? <span @click="showLogin = false">Sign Up</span> instead</p>
       </div>
       <div v-else>
         <h2>Sign Up</h2>
-        <SignupForm />
+        <SignupForm @signup="enterChat" />
         <p>Already registered? <span @click="showLogin = true">Login</span> instead</p>
       </div>
   </div>
@@ -18,13 +18,19 @@
 import SignupForm from '../components/SignupForm.vue'
 import LoginForm from '../components/LoginForm.vue'
 import { ref } from '@vue/reactivity'
+import { useRouter } from 'vue-router'
 
 export default {
     components: { SignupForm, LoginForm },
     setup() {
         const showLogin = ref(true)
+        const router = useRouter()
 
-        return { showLogin }
+        const enterChat = () => {
+            router.push({ name: 'Chatroom' })
+        }
+
+        return { showLogin, enterChat }
     }
 }
 </script>
